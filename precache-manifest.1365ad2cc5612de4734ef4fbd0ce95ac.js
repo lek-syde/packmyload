677ebcdab878337fbfd57798ea37b12a569d5f6a
alcuin2 <mail2oke@gmail.com>
2018-12-19 20:40:39 +0100

    Updates

---
+++ precache-manifest.1365ad2cc5612de4734ef4fbd0ce95ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "241c9fd1ea757090412c",
+    "url": "/packmyload/static/js/runtime~main.241c9fd1.js"
+  },
+  {
+    "revision": "d3443b74c279aba689e4",
+    "url": "/packmyload/static/js/main.d3443b74.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/packmyload/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "d3443b74c279aba689e4",
+    "url": "/packmyload/static/css/main.dd5c6ba6.chunk.css"
+  },
+  {
+    "revision": "4e0e176bcddeea87f7cf4886b9bb75bc",
+    "url": "/packmyload/index.html"
+  }
+];
